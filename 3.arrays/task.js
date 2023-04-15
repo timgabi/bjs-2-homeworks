bb75eed99264d42b971df67f2d62aa29fee48cf7
TimurGabitov1990 <gabi.tim1990@gmail.com>
2023-04-15 13:02:00 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
-  
+return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users.filter(user => gender === user.gender).map(user => user.age).reduce((acc, age, index, userAges) => acc + age / userAges.length, 0);
 }
